9e0539caf5c9057eeb7737c92e08195b9be1cd1e
kry-dev <yaroslavkryvda@gmail.com>
2020-02-05 20:06:18 +0200

    create completion page

---
+++ src/blocks/modules/completion/completion.js
@@ -0,0 +1,13 @@
+modules.define('completion', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
